8564d56dd31320206d4e1fea553af249e2b52b37
Nishant sharma <nishoo1902@gmail.com>
2023-07-23 15:48:08 +0530

    add google authentication2

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 // Web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBdYXhqSoduVHqVTcr3ydk-oArd4AKSvio",
-  authDomain: "calendar-78445.firebaseapp.com",
-  projectId: "calendar-78445",
-  storageBucket: "calendar-78445.appspot.com",
-  messagingSenderId: "999873188910",
-  appId: "1:999873188910:web:efc533c959c8d23fa97bbf",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBdYXhqSoduVHqVTcr3ydk-oArd4AKSvio",
+//   authDomain: "calendar-78445.firebaseapp.com",
+//   projectId: "calendar-78445",
+//   storageBucket: "calendar-78445.appspot.com",
+//   messagingSenderId: "999873188910",
+//   appId: "1:999873188910:web:efc533c959c8d23fa97bbf",
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
